cf89637adf2bb01b04b1681a0b61f3a22b77f484
Mijail Choque <choqueoliver35@gmail.com>
2023-03-02 10:06:37 +0100

    LAB 2

---
+++ HITO 2/LABORATORIOS/LAB 2/Math_utils/main.java
@@ -0,0 +1,37 @@
+package Math_utils;
+
+import java.util.*;
+
+public class main {
+
+	public static void main(String[] args) 
+	{
+		Scanner leer=new Scanner(System.in);
+		// TODO Auto-generated method stub
+		String nameAPP=leer.next();;
+		String version=leer.next();;
+		int year=leer.nextInt();
+		int limite;
+		String ci;
+		Math_utils mu = new Math_utils(nameAPP,version,year);
+		
+		mu.printAPP();
+		System.out.println();
+		
+		limite = leer.nextInt();
+		mu.generateNaturaleNumbers(limite);
+		
+		limite = leer.nextInt();
+		mu.generatePairNumbers(limite);
+		
+		ci = leer.next();
+		mu.generateExtensionFromCI(ci);
+		
+	
+	}
+
+
+
+	
+
+}
---
+++ HITO 2/LABORATORIOS/LAB 2/Student/main.java
@@ -0,0 +1,37 @@
+package Student;
+
+import java.util.*;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Scanner leer = new Scanner(System.in);
+		
+		student st = new student("Pepito","pep",25);
+		
+		String nombre = st.getFullName();
+		String apellido = st.getLastName();
+		int edad = st.getAge();
+		System.out.println(nombre + " " + apellido + " " + edad);
+		
+		st.setAge(30);
+		st.setFullName("pepito1");
+		st.setLastName("pep1");
+		
+		 nombre = st.getFullName();
+		 apellido = st.getLastName();
+		 edad = st.getAge();
+		
+		System.out.println(nombre + " " + apellido + " " + edad);
+		
+		
+	
+		
+		
+
+	}
+
+}
